d5344b62b1bcc184dd6c6372e44bd7a8ff9510e2
kchavinson <kc.chavinson@menloschool.org>
2024-11-18 09:42:47 -0800

    Finished Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
-
 public class Card {
+    private String rank;
+    private int value;
+    private String suit;
+
+    public Card(String rank, int value, String suit)
+    {
+        this.rank = rank;
+        this.value = value;
+        this.suit = suit;
+    }
+    public void setRank(String rank)
+    {
+        this.rank = rank;
+    }
+    public void setValue(int value)
+    {
+        this.value = value;
+    }
+    public void setSuit(String suit)
+    {
+        this.suit = suit;
+    }
+    public String getRank()
+    {
+        return this.rank;
+    }
+    public int getValue()
+    {
+        return this.value;
+    }
+    public String getSuit()
+    {
+        return this.suit;
+    }
+    public String toString()
+    {
+        return this.rank + " of " + this.suit;
+    }
+
 }
